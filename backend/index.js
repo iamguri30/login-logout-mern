a566bf5c4b04b6603fa6a1e42dda3b6419817933
iamguri-30 <gk5487942@gmail.com>
2024-10-05 08:07:08 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const app = express();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5000" }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+const uri = "mongodb+srv://admin:gOXcNwL2Ma1iUPfm@cluster0.0wtvf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// admin
+// gOXcNwL2Ma1iUPfm
+
+let db;
+let usersCollection;
+
+async function run() {
+      try {
+            // Connect the client to the server	(optional starting in v4.7)
+            await client.connect();
+            // Send a ping to confirm a successful connection
+            await client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+            // Specify the database and collection
+            db = client.db("Users"); // Replace 'myDatabase' with your database name
+            usersCollection = db.collection("collection_users"); // This creates the 'users' collection if it doesn't exist
+      } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+      }
+}
+run().catch(console.dir);
+
+
+app.post("/register", (req, res) => {
+      console.log(req.body)
+});
+
+if(process.env.NODE_ENV == "production"){
+      app.use(express.static("frontend/build"));
+}
+
+app.listen(PORT, () => {
+      console.log("server is running on port 5000");
+});
---
+++ client/src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react"; // Corrected the import
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const url = "http://localhost:5000/";
+      const [isSignUpActive, setIsSignUpActive] = useState(false);
+
+      const toggleForm = () => {
+            setIsSignUpActive(!isSignUpActive);
+      };
+
+      const registerUser = (e) => {
+            e.preventDefault();
+            let data = new FormData(e.target);
+            const user = {
+              username : data.get("username"),
+              email : data.get("email"),
+              password : data.get("pwd")
+            }
+
+            axios.post(url+"register", user).then((response) => {
+              alert(response)
+            }).catch((err) => {
+              alert(err)
+            })
+      };
+      return (
+            <>
+                  <section>
+                        <div className={`container ${isSignUpActive ? "active" : ""}`}>
+                              <div className="user signinBx">
+                                    <div className="imgBx">
+                                          <img src="https://raw.githubusercontent.com/WoojinFive/CSS_Playground/master/Responsive%20Login%20and%20Registration%20Form/img1.jpg" alt="" />
+                                    </div>
+                                    <div className="formBx">
+                                          <form onSubmit={(e) => e.preventDefault()}>
+                                                {" "}
+                                                {/* Corrected onSubmit */}
+                                                <h2>Sign In</h2>
+                                                <input autoComplete="Username" type="text" placeholder="Username" />
+                                                <input autoComplete="Password" type="password" placeholder="Password" />
+                                                <input autoComplete="Login" type="submit" value="Login" />
+                                                <p className="signup">
+                                                      Don't have an account? <a onClick={toggleForm}> Sign Up.</a>
+                                                </p>
+                                          </form>
+                                    </div>
+                              </div>
+
+                              <div className="user signupBx">
+                                    <div className="formBx">
+                                          <form onSubmit={registerUser}>
+                                                {" "}
+                                                {/* Corrected onSubmit */}
+                                                <h2>Create an account</h2>
+                                                <input autoComplete="Username" type="text" name="username" placeholder="Username" />
+                                                <input autoComplete="Email Address" type="email" name="email" placeholder="Email Address" />
+                                                <input autoComplete="Create Password" type="password" name="pwd" placeholder="Create Password" />
+                                                <input autoComplete="Confirm Password" type="password" name="c_pwd" placeholder="Confirm Password" />
+                                                <input type="submit" value="Sign Up" />
+                                                <p className="signup">
+                                                      Already have an account? <a onClick={toggleForm}> Sign in.</a>
+                                                </p>
+                                          </form>
+                                    </div>
+                                    <div className="imgBx">
+                                          <img src="https://raw.githubusercontent.com/WoojinFive/CSS_Playground/master/Responsive%20Login%20and%20Registration%20Form/img2.jpg" alt="" />
+                                    </div>
+                              </div>
+                        </div>
+                  </section>
+            </>
+      );
+}
+
+export default App;
